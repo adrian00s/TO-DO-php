d3dec06105f9d2aa0ed867186cc38e015f383cb8
Adrian Iordache <adiordache61@gmail.com>
2019-10-21 22:57:54 +0200

    finished design of index. Starting task php class

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>TO-DO List</title>
+
+  <!-- CSS -->
+  <link rel="stylesheet" href="css/index.css">
+  <link href="https://fonts.googleapis.com/css?family=Roboto+Mono&display=swap" rel="stylesheet"> 
+</head>
+<body>
+
+  <h1 class="title">TO-DO List PHP</h1>
+
+  <form action="" method="POST">
+    <div class="container">
+      <input type="text" name="addTask">
+      <button type="submit">ADD</button>
+    </div><!--end of container-->
+  </form>
+  <a href="" style="text-align: center; display:block; margin-top: 15px;">Retrieve task -></a>
+  <table>
+    <tr>
+      <td style="text-align: center;">ID</td>
+      <td style="text-align: center;">TASK</td>
+      <td>ACTIONS</td>
+    </tr>
+    <tr>
+      <td style="text-align: center;">1</td>
+      <td>Do homework</td>
+      <td style="text-align: center;">X</td>
+    </tr>
+    <tr>
+      <td style="text-align: center;">2</td>
+      <td>Make this project</td>
+      <td style="text-align: center;">X</td>
+    </tr>
+  </table>
+
+
+
+  
+</body>
+</html>
