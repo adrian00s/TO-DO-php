a1959b394faccf8d3e79e442c70c1e82d505473c
Adrian Iordache <adiordache61@gmail.com>
2019-10-23 02:09:25 +0200

    Almost finished front end. Fix Bug on task counter

--- index.php
+++ index.php
@@ -31,9 +31,12 @@
     </tr>
   </table>
 
+  <script
+			  src="https://code.jquery.com/jquery-3.4.1.min.js"
+			  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+        crossorigin="anonymous">
+  </script>   
   <script src="js/writeTask.js"></script>
-  <script src="js/removeTask.js"></script>
-      
   
 </body>
 </html>
--- js/writeTask.js
+++ js/writeTask.js
@@ -1,4 +1,4 @@
-(function(){
+(function($){
  
   let id = 0;
   const form =document.getElementById("taskForm");
@@ -12,17 +12,51 @@
       
     }else{
       table.innerHTML += `
-      <tr>
-        <td style="text-align: center;">${++id}</td>
-        <td>${this.getElementsByTagName("input")[0].value}</td>
-        <td style="text-align: center;"><button>X</button></td>
+      <tr class="task">
+        <td class="id" style="text-align: center;">${++id}</td>
+        <td class="taskName">${this.getElementsByTagName("input")[0].value}</td>
+        <td class="removeTd" style="text-align: center;"><button class="remove">X</button></td>
       </tr>
       `;
       this.getElementsByTagName("input")[0].value = "";
+      removeTask();
     }
 
     // Prevent default
     e.preventDefault();
   }
 
-}());
+  function removeTask(){
+    let getButtons = document.getElementsByClassName("remove");
+
+    for (let i = 0; i < getButtons.length; i++){
+
+      (function(i){
+
+        getButtons[i].onclick = function(){
+
+          let prevId = $(this).closest("tr").parent();
+          $(prevId).nextAll().each(function(i, tbody){
+            let tr = tbody.children[0];
+            tr.children[0].firstChild.nodeValue = Number(tr.children[0].firstChild.nodeValue)-1;
+            id--;
+          }); 
+
+          $(prevId).remove();
+
+        }
+      }(i));
+    }
+
+  }
+
+  function removeNextTd(button){
+    let getTableTr = document.getElementsByTagName("tr");
+
+    console.log(button)
+      console.log($(button).next("button.remove"));
+  }
+
+  
+
+}(jQuery));
